3deaf41e5f2e3060dfe803a85f4e3e8b716d8d10
Mandar <Mandar@LAPTOP-430QS8V3>
2022-07-19 16:17:05 +0530

    Add project files.

---
+++ ContactList/ContactList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactList
+{
+    class ContactList
+    {
+        private List<Contact> contacts { get; set; } = new List<Contact>();
+
+        private void DisplayDetails(Contact contact)
+        {
+            Console.WriteLine($"Contact: {contact.Name}, {contact.Number}");
+        }
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void DisplaySpecificContact(string number)
+        {
+            var contact = contacts.FirstOrDefault(c => c.Number == number);
+            if (contact == null)
+            {
+                Console.WriteLine("Contact Not Found");
+            }
+            else
+            {
+                DisplayDetails(contact);
+            }
+        }
+
+        public void DisplayAll()
+        {
+            foreach(var contact in contacts)
+            {
+                DisplayDetails(contact);
+            }
+        }
+
+        public void FindContact(string search)
+        {
+            var matchingContact = contacts.Where(c => c.Name.Contains(search)).ToList();
+            foreach(var contact in matchingContact)
+            {
+                DisplayDetails(contact);
+            }
+        }
+    }
+}
---
+++ ContactList/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ContactList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Contact List");
+            Console.WriteLine("Select Operation");
+            Console.WriteLine("1. Add Contact");
+            Console.WriteLine("2. Display Contact By Number");
+            Console.WriteLine("3. View All Numbers");
+            Console.WriteLine("4. Search for Contacts from a given name");
+            Console.WriteLine("Press x to Exit");
+
+            var input = Console.ReadLine(); 
+            
+            var contactList = new ContactList();
+
+            while(true)
+            {
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("Contact Name:");
+                        var name = Console.ReadLine();
+                        Console.WriteLine("Contact Number:");
+                        var number = Console.ReadLine();
+
+                        var newContact = new Contact(name, number);
+                        contactList.AddContact(newContact);
+
+                        break;
+                        
+                    case "2":
+                        Console.WriteLine("Contact Number to search:");
+                        var searchNumber = Console.ReadLine();
+                        contactList.DisplaySpecificContact(searchNumber);
+
+                        break;
+
+                    case "3":
+                        contactList.DisplayAll();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Name to Search:");
+                        var word = Console.ReadLine();
+
+                        contactList.FindContact(word);
+
+                        break;
+                    case "x":
+                        return;
+                    default:
+                        Console.WriteLine("Select Valid Operation");
+                        break;
+                }
+                Console.WriteLine("Select Operation");
+                input = Console.ReadLine();
+            }
+            
+        }
+    }
+}
